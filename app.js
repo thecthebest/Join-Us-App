b5f0213133ff44e350efd56e6cb4ccb7657d4c1b
thecthebest <you@example.com>
2021-12-08 16:35:23 +0000

    Added code for establishing connection
    Code for connecting to the database was inserted.

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
 //Packages
 const faker = require('faker');
 const mysql = require('mysql');
+
+//For connecting with database
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    database: 'mysql'
+});
